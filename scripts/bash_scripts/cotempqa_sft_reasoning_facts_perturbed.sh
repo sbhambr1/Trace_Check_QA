7f87b75cf85b843d794e953582bd007085885a43
Siddhant Bhambri <43840095+sbhambr1@users.noreply.github.com>
2025-11-14 12:02:39 -0800

    Include seed in experiment name and output directory

--- scripts/bash_scripts/cotempqa_sft_reasoning_facts_perturbed.sh
+++ scripts/bash_scripts/cotempqa_sft_reasoning_facts_perturbed.sh
@@ -26,7 +26,7 @@ SEED="${2:-42}"   # default to 42 if not provided
 
 python scripts/cotempqa_sft_reasoning_facts_perturbed.py \
     --model_id "$model_id" \
-    --expt_name "${model_name}-sft-cotempqa-reasoning-facts-perturbed-3ep" \
-    --output_dir "cotempqa/${model_name}-sft-adapter-reasoning-facts-perturbed" \
+    --expt_name "${model_name}-sft-cotempqa-reasoning-facts-perturbed-3ep-${SEED}" \
+    --output_dir "cotempqa/${model_name}-sft-adapter-reasoning-facts-perturbed-${SEED}" \
     --wandb_token $wandb_token
     --seed "$SEED"
